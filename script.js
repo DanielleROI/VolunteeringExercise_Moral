7c181c6de7d596fdababa7b2ad84e6ee479db854
Glitch (fullfilling) <none>
2021-02-07 16:29:56 +0000

    🍟🎪 Updated with Glitch

--- script.js
+++ script.js
@@ -4,41 +4,7 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
-
-
-window.addEventListener("DOMContentLoaded", setup);
-// window.addEventListener("resize", resize);
-function setup(){
-
-  document.getElementById("Communityevents").onmouseover = function() {
-  mouseOver();
-};
-document.getElementById("Communityevents").onmouseout = function() {
-  mouseOut();
-};
-
-function mouseOver() {
-  document.getElementById("Communityevents").style.color = "rgb(128,0,0)";
-}
-
-function mouseOut() {
-  document.getElementById("Communityevents").style.color = "black";
-}
-
-
-
-
-// react to page resize
-function resize(){
-	// get window width
-  var wid = window.innerWidth;
-  var hei = window.innerHeight;
-}
-}
-
-
-
-function myFunction() {
+function burgerBar() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -47,3 +13,75 @@ function myFunction() {
   }
 }
 
+window.addEventListener("DOMContentLoaded", setup);
+// window.addEventListener("resize", resize);
+function setup() {
+  document.getElementById("Communityevents").onmouseover = function() {
+    mouseOver();
+  };
+  document.getElementById("Communityevents").onmouseout = function() {
+    mouseOut();
+  };
+
+  function mouseOver() {
+    document.getElementById("Communityevents").style.color = "rgb(128,0,0)";
+  }
+
+  function mouseOut() {
+    document.getElementById("Communityevents").style.color = "black";
+  }
+
+  // react to page resize
+  function resize() {
+    // get window width
+    var wid = window.innerWidth;
+    var hei = window.innerHeight;
+  }
+}
+
+/* filter in the event pages  */
+
+filterSelection("all");
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+  }
+}
+
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("filterbtn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
