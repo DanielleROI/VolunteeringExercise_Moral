309151c3d7f84379ef684e94ba61ae5e27d15e92
Glitch (fullfilling) <none>
2021-02-07 11:13:50 +0000

    🌴🍬 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,49 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log("hi");
+
+
+
+window.addEventListener("DOMContentLoaded", setup);
+// window.addEventListener("resize", resize);
+function setup(){
+
+  document.getElementById("Communityevents").onmouseover = function() {
+  mouseOver();
+};
+document.getElementById("Communityevents").onmouseout = function() {
+  mouseOut();
+};
+
+function mouseOver() {
+  document.getElementById("Communityevents").style.color = "rgb(128,0,0)";
+}
+
+function mouseOut() {
+  document.getElementById("Communityevents").style.color = "black";
+}
+
+
+
+
+// react to page resize
+function resize(){
+	// get window width
+  var wid = window.innerWidth;
+  var hei = window.innerHeight;
+}
+}
+
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
